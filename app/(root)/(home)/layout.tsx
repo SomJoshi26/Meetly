4f703ae431585251a66d88f30c8cc9979dbb734f
Som Joshi <joshi.som10@gmail.com>
2026-06-18 23:38:28 +0530

    added images

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className='relative'>
-       Navbar
+       <Navbar />
 
        <div className='flex'>
-        Sidebar
+        <Sidebar />
 
                    <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28
                         max-md:pb-14 sm:px-14">
